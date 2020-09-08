f313448b87787b26a8b5a3d4ec615f33265e556c
pluto7073 <62442460+pluto7073@users.noreply.github.com>
2020-09-08 19:28:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function mods() {
+	document.location = "mods.html";
+}
+function home() {
+	document.location = "index.html";
+}
+function resourcepacks() {
+	document.location = "rp.html";
+}
+function datapacks() {
+	document.location = "datapacks.html";
+}
+function fwe() {
+	document.location = "flywithoutelytra.html";
+}
+function downloadFWE() {
+	document.location = "https://github.com/pluto7073/AlwaysFlying";
+}
+function downloadCoffee() {
+	document.location  = "https://github.com/pluto7073/Coffee";
+}
+function coffee() {
+	document.location = "coffee.html";
+}
