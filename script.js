5c7cd885c01d4c24299f7dd6aabdb7929107a038
pluto7073 <62442460+pluto7073@users.noreply.github.com>
2020-09-13 17:23:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,17 +17,35 @@ function downloadFWE() {
 	document.location = "https://github.com/pluto7073/AlwaysFlying/releases";
 }
 function downloadCoffee() {
-	document.location = "https://github.com/pluto7073/Coffee/releases";
+	document.location = "coffee.html#download";
 }
 function coffee() {
 	document.location = "coffee.html";
 }
 function downloadTea() {
-	document.location = "https://github.com/pluto7073/TeaMod/releases";
+	document.location = "teamod.html#download";
 }
 function tea() {
 	document.location = "teamod.html";
 }
 function reportBug() {
 	document.location = "https://forms.gle/7qrktSttGh28qn1n8";
+}
+function coffee1() {
+	document.location = "files/mods/coffee/coffee-1.16.2-1.2.jar";
+}
+function coffee2() {
+	document.location = "files/mods/coffee/coffee-1.16.3-1.2.jar";
+}
+function coffee3() {
+	document.location = "files/mods/coffee/coffee-1.16.3-1.3.jar";
+}
+function tea1() {
+	document.location = "files/mods/plutos-tea-mod/plutos-tea-mod-1.16.2-1.0.jar";
+}
+function tea2() {
+	document.location = "files/mods/plutos-tea-mod/plutos-tea-mod-1.16.3-1.0.jar";
+}
+function tea3() {
+	document.location = "files/mods/plutos-tea-mod/plutos-tea-mod-1.16.3-1.1.jar";
 }
